197fe904d483a59573908aa7c624e25e008c7259
ttran291 <ttran291@dxc.com>
2026-09-18 11:40:08 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -9,8 +10,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+                stream.flush();
             }
             Err(e) => {
                 println!("error: {}", e);
