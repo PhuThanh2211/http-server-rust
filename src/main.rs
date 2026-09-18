123d6790501170e4738585ca96a3cbe2bb255a4f
ttran291 <ttran291@dxc.com>
2026-09-18 11:53:09 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -11,8 +11,20 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
-                stream.flush();
+                let mut buf = [0u8; 1024];
+                let bytes_read = stream.read(&mut buf).unwrap();
+                let request = String::from_utf8_lossy(&buf[..bytes_read]);
+
+                let first_line = request.lines().next().unwrap_or("");
+                let path = first_line.split_whitespace().nth(1).unwrap_or("");
+
+                let response = match path {
+                    "/" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
+
+                let _ = stream.write_all(response.as_bytes());
+                let _ = stream.flush();
             }
             Err(e) => {
                 println!("error: {}", e);
