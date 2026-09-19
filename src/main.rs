4bc5daf61c68fc088746c43d60e596813acd80dc
ttran291 <ttran291@dxc.com>
2026-09-19 09:04:41 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,29 @@ use std::io::{Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 use std::thread;
+use std::env;
+use std::fs;
+use std::path::Path;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Redis Server listening here with port {}!!!", 4221);
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let mut directory = String::new();
+
+    for i in 0..args.len() {
+        if args[i] == "--directory" && i + 1 < args.len() {
+            directory = args[i + 1].clone();
+            break;
+        }
+    }
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                let dir = directory.clone();
                 thread::spawn(move || {
                     let mut stream = stream;
                     let mut buf = [0u8; 1024];
@@ -41,6 +54,19 @@ fn main() {
                             echo_str.len(),
                             echo_str
                         )
+                    } else if let Some(filename) = path.strip_prefix("/files/") {
+                        let file_path = Path::new(&dir).join(filename);
+
+                        if let Ok(contents) = fs::read(file_path) {
+                            format!(
+                                "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                                contents.len(),
+                                String::from_utf8_lossy(&contents)
+                            )
+                        } else {
+                            "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                        }
+
                     } else if path == "/user-agent" {
                         format!(
                             "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
