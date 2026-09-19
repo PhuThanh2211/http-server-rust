7430788d39923f12e99207de439e6bad3252fbca
ttran291 <ttran291@dxc.com>
2026-09-19 08:40:44 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::thread;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,43 +11,49 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut buf = [0u8; 1024];
-                let bytes_read = stream.read(&mut buf).unwrap();
-                let request = String::from_utf8_lossy(&buf[..bytes_read]);
+            Ok(stream) => {
+                thread::spawn(move || {
+                    let mut stream = stream;
+                    let mut buf = [0u8; 1024];
+                    let bytes_read = match stream.read(&mut buf) {
+                        Ok(n) if n > 0 => n,
+                        _ => return
+                    };
+                    let request = String::from_utf8_lossy(&buf[..bytes_read]);
 
-                let first_line = request.lines().next().unwrap_or("");
-                let path = first_line.split_whitespace().nth(1).unwrap_or("");
+                    let first_line = request.lines().next().unwrap_or("");
+                    let path = first_line.split_whitespace().nth(1).unwrap_or("");
 
 
-                let mut user_agent = "";
-                for line in request.lines() {
-                    if let Some(_val) = line.to_ascii_lowercase().strip_prefix("user-agent:") {
-                        user_agent = line["user-agent:".len()..].trim();
-                        break;
+                    let mut user_agent = "";
+                    for line in request.lines() {
+                        if let Some(_val) = line.to_ascii_lowercase().strip_prefix("user-agent:") {
+                            user_agent = line["user-agent:".len()..].trim();
+                            break;
+                        }
                     }
-                }
 
-                let response = if path == "/" {
-                    "HTTP/1.1 200 OK\r\n\r\n".to_string()
-                } else if let Some(echo_str) = path.strip_prefix("/echo/") {
-                    format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                        echo_str.len(),
-                        echo_str
-                    )
-                } else if path == "/user-agent" {
-                    format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                        user_agent.len(),
-                        user_agent
-                    )
-                } else {
-                    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
-                };
+                    let response = if path == "/" {
+                        "HTTP/1.1 200 OK\r\n\r\n".to_string()
+                    } else if let Some(echo_str) = path.strip_prefix("/echo/") {
+                        format!(
+                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                            echo_str.len(),
+                            echo_str
+                        )
+                    } else if path == "/user-agent" {
+                        format!(
+                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                            user_agent.len(),
+                            user_agent
+                        )
+                    } else {
+                        "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                    };
 
-                let _ = stream.write_all(response.as_bytes());
-                let _ = stream.flush();
+                    let _ = stream.write_all(response.as_bytes());
+                    let _ = stream.flush();
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
